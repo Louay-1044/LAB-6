67fd66ea189af150c77e9c36b4cb5ca6e9632e9f
Your NameSystem.out.println(Hello from main branch!) <you@example.com>
2025-02-27 17:26:11 +0000

    Update message in main branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-      	System.out.println("Hello from conflict-branch!");
+      	System.out.println("Hello from main branch!");
+
 
     }
 }
